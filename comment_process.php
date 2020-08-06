6b599e29b3910226b775c304d906d4a6840d2555
shtmd6171 <samantha.n.morton@gmail.com>
2020-08-06 13:56:38 +0900

    코멘트 구현

---
+++ comment_process.php
@@ -0,0 +1,23 @@
+<?php
+include "./lib/db.php";
+
+$user_id = $_POST['user_id'];
+$review_id = $_POST['review_id'];
+$comm_description = $_POST['desc'];
+var_dump($user_id);
+var_dump($review_id);
+var_dump($comm_description);
+
+
+if(isset($user_id)&&isset($review_id)&&isset($comm_description)){
+$sql = mq("insert into comment (comm_description,review_id,user_id) values('".$comm_description."','".$review_id."','".$user_id."')");
+ echo "<script>window.location = 'review_board.php?review=$review_id'</script>";
+} else {
+  echo mysqli_error($db);
+	echo "<script>alert(\"NO.\");</script>";
+	echo "<script>window.location = 'review_board.php?review=$review_id'</script>";
+}
+
+
+
+ ?>
