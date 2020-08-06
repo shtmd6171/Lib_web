0419e69a2bed30d09cb2b10f939b66e64d9b590a
shtmd6171 <samantha.n.morton@gmail.com>
2020-08-07 00:20:54 +0900

    작성 조건 수정!

--- review_write_process.php
+++ review_write_process.php
@@ -8,7 +8,7 @@ $user_id = $_POST['user_id'];
 
 
 
-if(isset($user_id)&&isset($book_id)&&isset($review_title)&&isset($review_desc)){
+if(!($review_title==""||$review_desc==""||$book_id==""||$user_id=="")){
 $sql = mq("insert into book_review (review_title,review_desc,book_id,user_id) values('".$review_title."','".$review_desc."','".$book_id."','".$user_id."')");
  echo "<script>alert('OK.')</script>";
  echo "<script>window.location = './review.php?id=$book_id'</script>";
--- write_process.php
+++ write_process.php
@@ -12,14 +12,16 @@ $filename = iconv("UTF-8","EUC-KR",$_FILES['lo_image_link']['name']);
 $folder = "./file/".$filename;
 $vall = move_uploaded_file($tmpfile,$folder);
 
-if(isset($title)&&isset($author)&&isset($publisher)&&isset($date)&&isset($genre)){
+if(!($title==""||$author==""||$publisher==""||$date==""||$genre=="")){
 $sql = mq("insert into book (title,author,publisher,the_date,genre,file) values('".$title."','".$author."','".$publisher."','".$date."','".$genre."','".$o_name."')");
  echo "<script>alert('OK.')</script>";
- //echo "<script>window.location = '../viewAll.php'</script>";
+ var_dump($title);
+ var_dump($author);
+ echo "<script>window.location = './book_list.php'</script>";
 } else {
   echo mysqli_error($db);
 	echo "<script>alert(\"NO.\");</script>";
-	//echo "<script>window.location = '../writepage.php'</script>";
+	echo "<script>window.location = './wirte.php'</script>";
 }
 
 
