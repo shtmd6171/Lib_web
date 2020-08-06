5088fc3520480765bed6d6da125eb65aa58b7f90
shtmd6171 <samantha.n.morton@gmail.com>
2020-08-07 00:20:17 +0900

    JQuery파일 추가

---
+++ javascr/onClickFunc.js
@@ -0,0 +1,41 @@
+
+  $(function() {
+    $(".s").on('click',function() {
+      $("#mform").submit();
+    });
+    $(".m").on('click',function() {
+      $(location).attr('href',"./member.php" );
+    });
+
+    $("#chk").on('click',function(){
+      var id = $("#uid").val();
+      if(id){
+        id = "check.php?userid="+id;
+        window.open(id,"_blank","width=300,height=100");
+        }else{
+          alert("아이디를 입력세요");
+        }
+    });
+
+    $(".r").on('click',function() {
+      $("#mform").submit();
+    });
+
+    $(".re").on('click',function() {
+      $(location).attr('href',"./member.php" );
+    });
+
+    $(".b").on('click',function() {
+      $(location).attr('href',"./login.php" );
+    });
+
+    $('.wb').hover(function() {
+    $(this).css('cursor','Pointer');
+    });
+
+    $('.wb').children("a").css('text-decoration','none').css('color','black');
+
+    $("a").css('text-decoration','none').css('color','black');
+
+
+  });
