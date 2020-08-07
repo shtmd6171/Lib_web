82ff346e4c1d4909fa1366b5383741a94882565d
shtmd6171 <samantha.n.morton@gmail.com>
2020-08-07 11:35:10 +0900

    책 목록 삭제하는 기능(삭제시 서평과 댓글도 같이)

---
+++ book_delete_process.php
@@ -0,0 +1,20 @@
+<?php
+include "./lib/db.php";
+
+$book_id = $_GET['id'];
+
+if(!($book_id==""&&(!(isset($book_id))))){
+$sql = mq("delete from book where book_id ='".$book_id."'");
+$sql_review_getID = mq("select * from book_review where book_id ='".$book_id."'");
+$getID = $sql_review_getID->fetch_array();
+$sql_review = mq("delete from book_review where book_id='".$book_id."'");
+$sql_comment = mq("delete from comment where review_id='".$getID['review_id']."'");
+ echo "<script>alert('OK.')</script>";
+ echo "<script>window.location = './book_list.php'</script>";
+} else {
+  echo mysqli_error($db);
+	echo "<script>alert(\"NO.\");</script>";
+	echo "<script>window.location = './book_list.php'</script>";
+}
+
+  ?>
