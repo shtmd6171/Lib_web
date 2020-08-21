fd29050f1916ddaa82707d1471bdf4cc376d98a6
unknown <seonar138@naver.com>
2020-08-21 15:47:32 +0900

    save point no.1

--- log/login.php
+++ log/login.php
@@ -13,95 +13,47 @@ include "../lib/db.php";
 <head>
     <meta charset="utf-8"/>
     <title></title>
-	<link
-        rel="stylesheet"
-        href="../css/bootstrap.css">
-	<link
-        rel="stylesheet"
-        href="../css/bootstrap-theme.css">
-	<script
-        src="../css/js/bootstrap.js"></script>
-    <script src="../javascr/onClickFunc.js"></script>
+	<link rel="stylesheet" href="../css/bootstrap.css">
+	<link rel="stylesheet" href="../css/bootstrap-theme.css">
+	<script src="../css/js/bootstrap.js"></script>
+	<!-- <script src="../javascr/onClickFunc.js"></script> -->
+
 	<style>
-		.flex_container {
-  		display: flex;
-		}
-	</style>
+	.flex_container {
+	  display: flex;
+	}
+
+	.flex-item {
+  	flex-grow: 1;
+  	flex-shrink: 1;
+  	flex-basis: auto;
+	}
+</style>
+
 </head>
-<br>
-<br>
-<br>
-<div id="container">
-	<div class="">
-	<img src="../file/login_1.jpg" class="rounded mx-auto d-block" alt="...">
-	</div>
-			<form id="mform" method="post" action="./login_ok.php">
-				<table align="center" border="0" cellspacing="0" width="300">
-					<tr>
-						<td >
-							<div>
-							<h1 align="center">Login</h1>
-					 	</td>
-					</tr>
-					<tr>
-						<td>
-							<div class="form-group">
-            				<label for="inputEmail">Email address</label>
-            				<input  type="email" 
-									class="form-control" 
-									name="email">
-							</div>
-						</td>
-					</tr>
-					<!--
-					<tr>
-						<td colspan="10">
-							<div class="line">	</div>
-						</td>
-					</tr>
-						<tr>
-							<td colspan="2">e-mail</td>
-							<td align="center">
-	              <input type="email" name="email">
-							</td>
-						</tr>-->
-					<tr>
-						<td>
-							<div class="form-group">
-            				<label for="inputPwd">Password</label>
-            				<input  type="password" 
-									class="form-control" 
-									name="pwd">
-							</div>
-						</td>
-					</tr>
-					 <!--<tr>
-						<td colspan="2">Password</td>
-						<td align="center">
-							<input type="password" name="pwd">
-						</td>
-					</tr>
-					-->
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col -sm 6 login-section-wrapper">
+				<div class="login-wrapper my -auto">
+					<h1 class="login-title"> Log in </h1>
+					<form id="mform" method="post" action="./login_ok.php">
+						<div class="form-group">
+							<label for="email">Email address</label>
+							<input type="email" name="email" id="email" class="form-control"
+							placeholder="email@example.com">
 
-					<tr>
-						<td>
-						<br>
-						</td>
-					</tr>
-
-					<tr>
-						<td align="center" colspan="5">
-							<div>
-							<div type="button" class="btn btn-outline-secondary s" id="btn">Sign in</div>
-							 <!--class="mine s" id="btn">Sign in</div> -->
-						</td>
-						<td align="center" colspan="5">
-						 <!--class="mine m">Create Account-->
-							<div type="button" class="btn btn-outline-secondary m">Create Account</div>
-						</td>
-					</tr>
-				</table>
-			</form>
+							<label for="pwd">Password</label>
+							<input type="password" name="pwd" id="pwd" class="form-control">
+						</div>
+						<div>
+							<button class="s"> Sign in</button>
+							<button class="s"> Create account</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
 	</div>
 </body>
 </html>
