126f374705b93acdc480fcb756333c63f74676ea
shtmd6171 <samantha.n,morton@gmail.com>
2020-09-02 17:55:04 +0900

    -

--- branch_hak/loan_list.php
+++ branch_hak/loan_list.php
@@ -7,15 +7,13 @@ $nowDate = $date->format('Y-m-d H:i:s');
 
 //LOAN 테이블에서 유저아이디를 하나의 행으로 조회한다.
 $sql = mq("select * from loan where user_id ='".$user_id."' ");
-$sql->fetch_array();
 
-$list = mq("SELECT title,author,publisher, loan_date as 빌린날짜, genre, file, return_date from book, loan where book.book_id = loan.book_id and loan.user_id ='".$user_id."'");
-$loancheck = $list->fetch_array();
+$list = mq("select title,author,publisher, loan_date as 빌린날짜, genre, file, return_date from book, loan where book.book_id = loan.book_id and loan.user_id ='".$user_id."'");
 
-if(($nowDate - $loancheck['return_date']) == 0 )
-{
-  $delete = mq("DELETE FROM loan WHERE book.book_id = loan.book_id and loan.user_id ='".$user_id."'");
-}
+// if(($nowDate - $loancheck['return_date']) == 0 )
+// {
+//   $delete = mq("DELETE FROM loan WHERE book.book_id = loan.book_id and loan.user_id ='".$user_id."'");
+// }
 
 ?>
 
@@ -35,13 +33,16 @@ if(($nowDate - $loancheck['return_date']) == 0 )
           <h1>책 대여 리스트</h1>
         </div>
 
-        <?php if ($sql->num_rows > 0) {
-          while($result = $list->fetch_array())
-          {?>
-        <div class="card" style="width: 18rem;"> <!--일단 카드형으로 만들어보긴 했는데, 메인 페이지처럼 갯수별로 불러오는 백엔드 할 수 있을까요?-->
-          <img class="card-img-top" src="../file/<?= $result['file']; ?>" alt="Card image cap" width="250" height="250">
+        <?php $value = $sql->num_rows ;
+        if ($value >= 0 ) {
+          while( $result = $list->fetch_array()) { ?>
+
+        <div class="card" style="width: 18rem;">
+          <!--일단 카드형으로 만들어보긴 했는데, 메인 페이지처럼 갯수별로 불러오는 백엔드 할 수 있을까요?-->
+          <img class="card-img-top" src="../file/resize/resizing_<?=$result['file'];  ?>" alt="Card image cap" width="250" height="250">
           <div class="card-body">
             <h5 class="card-title"><?=$result['title'] ?></h5>
+            <p><?php echo $result['file'];  ?></p>
             <p class="card-text"><?=$result['author'] ?></p>
             <p class="card-text"><?=$result['publisher'] ?></p>
             <p class="card-text"><?=$result['빌린날짜'] ?></p>
@@ -49,7 +50,7 @@ if(($nowDate - $loancheck['return_date']) == 0 )
             <a href="#" class="btn btn-primary">반납하기</a>
           </div>
         </div>
-      <?php   }  } else {
+      <?php  $value--; }  } else {
        echo "대출 리스트가 존재하지 않습니다.";
      } ?>
 
