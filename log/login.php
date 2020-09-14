e8f4f6aa98031c6cfc0ad7b2ad930757612caf31
unknown <seonar138@naver.com>
2020-09-14 17:15:36 +0900

    login css 2

--- log/login.php
+++ log/login.php
@@ -1,7 +1,7 @@
 <?php
 include "../lib/db.php";
 ?>
-
+<!-- https://nanati.me/css-button-design/ 버튼 디자인 연구-->
 <!DOCTYPE html>
 <html lang="en" >
 
@@ -32,12 +32,12 @@ include "../lib/db.php";
       <div class="row">
         <div class="row col-12 mrow">
 
-          <div class="col-6 content my-auto ml-9 d-none d-sm-block">
+          <div class="col-sm-6 col-md-6 content my-auto ml-9 d-none d-sm-block">
             <div class="login-wrapper">
-              <div id="L_title" class="mb-3"> LOG-IN </div>
+              <div id="L_title" class="mb-3"> LOGIN </div>
               <form id="mform" method="post" action="./login_ok.php" style="z-index:9999;">
                 <div class="form-group">
-                  <div class="row hhhhh mb-3">
+                  <div class="row mb-3">
                     <label id="L_login" for="email">Email address</label>
                     <input type="email" name="email" id="email" class="form-control"
                     placeholder="email@example.com" required>
@@ -48,15 +48,26 @@ include "../lib/db.php";
                   </div>
                 </div>
                 <div class="row mb-3">
-                  <input type="submit" value="sign in" class="btn btn-outline-primary">
+                  <input type="submit" value="sign in" class="btn btn-dark mt-4 btn-lg btn-block L-btn" active>
+                  <!-- <input type="submit" value="sign in" class="btn btn-dark mt-4 btn-lg btn-block" active> -->
+                  <!-- 블록레벨 버튼으로 할지 그냥 라지버튼으로 할지 함께 상의할것! -->
                 </div>
               </form>
-              <button class="btn btn-outline-primary" onclick="location.href='../log/member.php'">Create account</button>
-              <button class="btn btn-outline-primary" onclick="location.href='../branch_hak/password_find.php'">forgot password</button>
+              <div class="row">
+                계정이 없으신가요?
+                <div class="row">
+                  <a href="../log/member.php">Create account</a>
+
+                </div>
+
+              </div>
+              <div class="row">
+                <span>비밀번호를 잃어버리셨나요?
+                  <a href="../branch_hak/password_find.php">Find your Password</a>
+                </span>
+            </div>
             </div>
           </div>
-
-
 
         </div>
       </div>
